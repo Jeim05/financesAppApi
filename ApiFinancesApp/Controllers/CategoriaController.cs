f039e6a99385cb95384de284f34b1e935a06b636
Jeim05 <lopezvegajeimmy@gmail.com>
2024-12-19 22:27:14 -0600

    Crear Get para categorias

---
+++ ApiFinancesApp/Controllers/CategoriaController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CapaDatos.Customs;
+using CapaEntidad;
+
+namespace ApiFinancesApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriaController : ControllerBase
+    {
+        private readonly DbPersonalfinancesContext _context;
+        public CategoriaController(DbPersonalfinancesContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Lista()
+        {
+            var lista = await _context.Categoria.ToListAsync();
+            return StatusCode(StatusCodes.Status200OK, new {value=lista});
+        }
+    }
+}
