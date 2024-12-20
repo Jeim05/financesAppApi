d5a92af8414d2ca8be9dc42062eb66767182919e
Jeim05 <lopezvegajeimmy@gmail.com>
2024-12-19 22:12:02 -0600

    Agregar utilidades para JWT

---
+++ CapaDatos/Customs/Utilidades.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+using CapaEntidad;
+using Microsoft.Extensions.Configuration;
+
+namespace CapaDatos.Customs
+{
+    public class Utilidades
+    {
+        private readonly IConfiguration _configuration;
+
+        public Utilidades(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+    }
+}
